ee1e38284c81d14e284a663c08cf4d551d4a64f4
Tom Seb <uriel@example.com>
2024-01-08 15:46:25 -0800

    Created test Jenkinsfile

--- project/web_service.py
+++ project/web_service.py
@@ -1,9 +1,16 @@
+"""
+This is a basic webservice
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    """
+    A simple hello world route
+    """
     return 'Hello, World!'
 
 if __name__ == '__main__':
